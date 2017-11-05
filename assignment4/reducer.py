198e248ded0ef957facfd06cceff2ccccaec9bd8
Mohammad Julfikar Rahman (Shemul) <bsse0640@iit.du.ac.bd>
2017-11-05 11:54:15 +0600

    Update reducer.py

---
+++ assignment4/reducer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+from urlparse import urlparse
+
+path_dictonary = {}
+
+for line in sys.stdin:
+	data = line.strip().split("\t")
+	
+	path = data[0]
+    	url = urlparse(path).path
+     
+    	if not url in path_dictonary:
+        	path_dictonary[url] = 1
+    	else:
+        	path_dictonary[url] += 1
+        
+count, max_hitted_path = max(zip(path_dictonary.values(), path_dictonary.keys()))
+
+print count
